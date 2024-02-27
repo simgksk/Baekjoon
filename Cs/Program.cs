a0c36baeba5d327ec7939375ad28321c244e159b
하나 <123716081+simgksk@users.noreply.github.com>
2024-02-28 05:08:25 +0900

    Add files via upload

---
+++ Cs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _230425_Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("42");
+            Console.WriteLine("iz3");
+        }
+    }
+}
