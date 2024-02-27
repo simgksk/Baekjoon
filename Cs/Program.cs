6254f5ca0766bb9efbfee9c5be9481c66b4d24ca
하나 <123716081+simgksk@users.noreply.github.com>
2024-02-28 04:52:03 +0900

    Add files via upload

---
+++ Cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _230423_Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int length = s.Length;
+
+            Console.WriteLine(length);
+                   
+        }
+    }
+}
