edfea070177ca94474bf227b29cd0b807cb7dec2
하나 <123716081+simgksk@users.noreply.github.com>
2024-02-28 04:54:43 +0900

    Add files via upload

---
+++ Cs/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace _230622_Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = short.Parse(Console.ReadLine());
+            short b = short.Parse(Console.ReadLine());
+
+            Console.WriteLine(a+b);
+            Console.WriteLine(a-b);
+            Console.WriteLine(a*b);
+        }
+    }
+}
