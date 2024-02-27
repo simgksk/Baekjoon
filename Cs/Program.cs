d849093227dce662061b79b38c685355f4c7d6d7
하나 <123716081+simgksk@users.noreply.github.com>
2024-02-28 05:09:34 +0900

    Add files via upload

---
+++ Cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _230502_Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            if (a < b && a > c) Console.WriteLine(a); //a
+            else if (a > b && a < c) Console.WriteLine(a); //a
+            else if (a < b && b > c) Console.WriteLine(b); //b
+            else if (a > b && b < c) Console.WriteLine(b); //b
+            else if (c < b && c > a) Console.WriteLine(c); //c
+            else if (c > b && c < a) Console.WriteLine(c); //c
+        }
+    }
+}
