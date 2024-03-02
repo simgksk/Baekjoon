dfb7d24de8d9337f9c0cc603ad8b1cf1c61502df
하나 <123716081+simgksk@users.noreply.github.com>
2024-03-02 23:47:07 +0900

    Add files via upload

---
+++ Cpp/240302.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+using namespace std;
+
+	int main()
+	{
+#pragma region 10170. NFC West vs North
+
+		cout << "NFC West       W   L  T" << endl;
+		cout << "-----------------------" << endl;
+		cout << "Seattle        13  3  0" << endl;
+		cout << "San Francisco  12  4  0" << endl;
+		cout << "Arizona        10  6  0" << endl;
+		cout << "St. Louis      7   9  0" << endl;
+		cout << endl;
+		cout << "NFC North      W   L  T" << endl;
+		cout << "-----------------------" << endl;
+		cout << "Green Bay      8   7  1" << endl;
+		cout << "Chicago        8   8  0" << endl;
+		cout << "Detroit        7   9  0" << endl;
+		cout << "Minnesota      5  10  1" << endl;
+
+#pragma endregion
+
+#pragma region 11367. Report Card Time
+
+		int n, grade;
+		string name;
+
+		cin >> n;
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> name >> grade;
+
+			if (grade <= 59)
+				cout << name << " F" << endl;
+			else if (grade <= 66)
+				cout << name << " D" << endl;
+			else if (grade <= 69)
+				cout << name << " D+" << endl;
+			else if (grade <= 76)
+				cout << name << " C" << endl;
+			else if (grade <= 79)
+				cout << name << " C+" << endl;
+			else if (grade <= 86)
+				cout << name << " B" << endl;
+			else if (grade <= 89)
+				cout << name << " B+" << endl;
+			else if (grade <= 96)
+				cout << name << " A" << endl;
+			else if (grade <= 100)
+				cout << name << " A+" << endl;
+		}
+
+#pragma endregion
+
+#pragma region 16430. 제리와 톰
+
+		int a, b;
+
+		cin >> a >> b;
+
+		cout << b - a << " " << b;
+
+#pragma endregion
+
+		return 0;
+	}
+
