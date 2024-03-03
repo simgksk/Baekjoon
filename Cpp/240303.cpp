62217a7769618ab343361fe457f5514aafd8c367
하나 <123716081+simgksk@users.noreply.github.com>
2024-03-03 21:54:21 +0900

    Add files via upload

---
+++ Cpp/240303.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+	int main()
+	{
+#pragma region 11718. 그대로 출력하기
+
+		ios_base::sync_with_stdio(false);
+		cin.tie(NULL);
+
+		string s;
+
+		while (true)
+		{
+			getline(cin, s);
+
+			if (s == "")
+				break;
+
+			cout << s << "\n";
+		}
+
+#pragma endregion
+
+		return 0;
+	}
