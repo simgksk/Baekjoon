47b5027f370090abc3f46f45d114a2faf6e43069
하나 <123716081+simgksk@users.noreply.github.com>
2024-03-09 23:53:02 +0900

    Add files via upload

---
+++ Cpp/소스.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 30007. 라면 공식
+
+	/*int n, a, b, x;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b >> x;
+		int w = a * (x - 1) + b;
+
+		cout << w << "\n";
+	}*/
+
+#pragma endregion
+
+#pragma region 30030. 스위트콘 가격 구하기
+
+	int b;
+
+	cin >> b;
+
+	cout << 10 * (b / 11);
+
+#pragma endregion
+
+	return 0;
+}
