17ed941b180ffc8da91069d0606470bca4d736bb
하나 <123716081+simgksk@users.noreply.github.com>
2024-03-10 21:16:37 +0900

    Add files via upload

---
+++ Cpp/소스.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+#pragma region 15552. 빠른 A+B
+
+	cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+
+	int t, a, b;
+
+	cin >> t;
+
+	while (t--)
+	{
+		cin >> a >> b;
+		cout << a + b << "\n";
+	}
+
+#pragma endregion
+
+#pragma region 1264. 모음의 개수
+
+	string s;
+
+	while (true)
+	{
+		int cnt = 0;
+
+		getline(cin, s);
+
+		if (s == "#")
+			break;
+
+		for (int i = 0; i < s.length(); i++)
+		{
+			if (s[i] == 'a' || s[i] == 'A')
+				cnt++;
+			if (s[i] == 'e' || s[i] == 'E')
+				cnt++;
+			if (s[i] == 'i' || s[i] == 'I')
+				cnt++;
+			if (s[i] == 'o' || s[i] == 'O')
+				cnt++;
+			if (s[i] == 'u' || s[i] == 'U')
+				cnt++;
+		}
+		cout << cnt << "\n";
+	}
+
+#pragma endregion
+
+	return 0;
+}
