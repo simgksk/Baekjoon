6f48b398b468b049295ec396d7fb918247731b18
하나 <123716081+simgksk@users.noreply.github.com>
2024-03-11 22:49:45 +0900

    Add files via upload

---
+++ Cpp/소스.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 1000. A+B
+
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a + b;
+
+#pragma endregion
+
+#pragma region 1001. A-B
+
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a - b;
+
+#pragma endregion
+
+	return 0;
+}
