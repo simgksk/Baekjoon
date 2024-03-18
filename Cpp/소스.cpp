2366bd36ce395672151e9e70021a71a5e7e79b63
하나 <123716081+simgksk@users.noreply.github.com>
2024-03-18 14:27:50 +0900

    Add files via upload

---
+++ Cpp/소스.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 31614. Minutes
+
+	int h, m;
+
+	cin >> h >> m;
+
+	cout << h * 60 + m;
+
+#pragma endregion
+
+	return 0;
+}
