7e40efa4bb8bcf22f4969292cd1cac97ca6deda3
하나 <123716081+simgksk@users.noreply.github.com>
2024-04-19 22:30:38 +0900

    Add files via upload

---
+++ Cpp/0_2xxx/2675.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	//2675. 문자열 반복
+
+	int t, p;
+	string s;
+
+	cin >> t;
+
+	while (t--)
+	{
+		cin >> p >> s;
+
+		for (int i = 0; i < s.length(); i++)
+		{
+			for (int j = 0; j < p; j++)
+				cout << s[i];
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
