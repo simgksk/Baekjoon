b11901d5c95d431ba8730b30302192f59a9944c5
하나 <123716081+simgksk@users.noreply.github.com>
2024-04-20 23:24:23 +0900

    Add files via upload

---
+++ Cpp/26xxx/소스1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//26766. Serca
+	int n;
+
+	cin >> n;
+
+	while (n--)
+	{
+		cout << " @@@   @@@ \n";
+		cout << "@   @ @   @\n";
+		cout << "@    @    @\n";
+		cout << "@         @\n";
+		cout << " @       @ \n";
+		cout << "  @     @  \n";
+		cout << "   @   @   \n";
+		cout << "    @ @    \n";
+		cout << "     @     \n";
+	}
+
+	return 0;
+}
