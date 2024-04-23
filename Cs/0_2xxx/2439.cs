724d84f73a18975f9c639b5a1770e4050ca1288b
하나 <123716081+simgksk@users.noreply.github.com>
2024-04-23 10:41:26 +0900

    2439. 별 찍기 - 2

--- Cs/0_2xxx/2439.cs
+++ Cs/0_2xxx/2439.cs
@@ -12,13 +12,11 @@ namespace _2439
             for (int i = 0; i <a; i++)
             {
                  for(int j=0; j<(a-i-1); j++)
-                {
                     Console.Write(" ");
-                }
+                
                  for(int k=0; k<=i; k++)
-                {
                     Console.Write("*");
-                }
+                
                 Console.WriteLine();
             }
             
