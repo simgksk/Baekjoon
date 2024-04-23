d72589244567542ab8715f5220de6cfec8fdd67a
하나 <123716081+simgksk@users.noreply.github.com>
2024-04-23 10:53:47 +0900

    10718. We love kriii

--- Cs/10xxx/10718.cs
+++ Cs/10xxx/10718.cs
@@ -6,10 +6,8 @@ namespace _10718
     {
         static void Main(string[] args)
         {
-            
             Console.WriteLine("강한친구 대한육군");
             Console.WriteLine("강한친구 대한육군");
-            
         }
     }
 }
