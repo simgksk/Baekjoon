1d05ea586f67076fe2d01e78403eaf4dc4486aa0
하나 <123716081+simgksk@users.noreply.github.com>
2024-04-23 11:09:49 +0900

    4714. Lunacy

---
+++ Cpp/0_4xxx/4714.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 4714. Lunacy
+
+	float x;
+
+	while (true)
+	{
+		cin >> x;
+
+		float y = x * 0.167;
+
+		if (x < 0)
+			break;
+
+		printf("Objects weighing %.2f on Earth will weigh %.2f on the moon.\n", x, y);
+	}
+		
+
+#pragma endregion
+
+	return 0;
+}
