c049a28551975fa477f15d41adce9669cb3aa0ed
하나 <123716081+simgksk@users.noreply.github.com>
2024-04-30 10:33:27 +0900

    2164. 카드2

---
+++ Cpp/0_2xxx/2164.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	queue<int> q;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+		q.push(i);
+
+	while (q.size() > 1)
+	{
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+
+	cout << q.front();
+
+	return 0;
+}
