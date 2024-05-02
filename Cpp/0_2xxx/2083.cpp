804ecc6b4a501b91626ffdb442e86e8f2702e983
하나 <123716081+simgksk@users.noreply.github.com>
2024-05-02 11:44:07 +0900

    2083. 럭비 클럽
    브론즈 4

--- Cpp/0_2xxx/2083.cpp
+++ Cpp/0_2xxx/2083.cpp
@@ -2,22 +2,22 @@
 
 using namespace std;
 
-    int main()
+int main()
+{
+    int age, weight;
+    string name;
+
+    while (true)
     {
-        int age, weight;
-        string name;
+        cin >> name >> age >> weight;
 
-        while (true)
-        {
-            cin >> name >> age >> weight;
-
-            if (name == "#" && age == 0 && weight == 0)
-                break;
-            if (age > 17 || weight >= 80)
-                cout << name << " Senior" << endl;
-            else
-                cout << name << " Junior" << endl;
-        }
-
-        return 0;
+        if (name == "#" && age == 0 && weight == 0)
+            break;
+        if (age > 17 || weight >= 80)
+            cout << name << " Senior" << endl;
+        else
+            cout << name << " Junior" << endl;
     }
+
+     return 0;
+}
