3ac44994212972d5cd7d0b7dbc66dd97f104aa30
하나 <123716081+simgksk@users.noreply.github.com>
2024-05-25 23:57:55 +0900

    25083. 새싹
    브론즈 5

---
+++ Cpp/25xxx/25083.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//25083. »õ½Ï
+	cout << "         ,r'\"7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |\n";
+
+	return 0;
+}
