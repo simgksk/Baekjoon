3d7342dc940b55324fbee1f52260f1fbd6f4335e
하나 <123716081+simgksk@users.noreply.github.com>
2024-06-03 19:48:37 +0900

    10816. 숫자 카드 2
    실버 4

---
+++ Cpp/10xxx/10816.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, num, m;
+	vector<int> v;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end());
+
+	cin >> m;
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> num;
+
+		int cnt = upper_bound(v.begin(), v.end(), num) - lower_bound(v.begin(), v.end(), num);
+
+		cout << cnt << " ";
+	}
+
+	return 0;
+}
