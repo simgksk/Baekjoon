51adccf65621cc1970ba7898ff7831c133762377
simgksk <123716081+simgksk@users.noreply.github.com>
2025-01-14 18:19:39 +0900

    10953. A + B - 6
    브론즈 3

---
+++ Cpp/10xxx/10953.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t, a, b;
+	char c;
+
+	cin >> t;
+
+	while (t--){
+		cin >> a >> c >> b;
+		cout << a + b << "\n";
+	}
+
+	return 0;
+}
