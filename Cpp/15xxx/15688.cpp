f1c52fc41b23f8d5d002d9f38bae9f72cfc6d5f5
simgksk <123716081+simgksk@users.noreply.github.com>
2025-01-15 13:27:27 +0900

    15688. 수 정렬하기 5
    실버 5

---
+++ Cpp/15xxx/15688.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int n, num;
+	vector <int> v;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << "\n";
+
+	return 0;
+}
