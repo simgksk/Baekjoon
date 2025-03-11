aa93c745ca6a358eef423426f35b24169f201cd7
simgksk <123716081+simgksk@users.noreply.github.com>
2025-03-12 08:48:52 +0900

    14682. Shifty Sum
    https://www.acmicpc.net/problem/14682

---
+++ Cpp/14xxx/14682.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, k;
+
+	cin >> n >> k;
+
+	int sum = n;
+	for (int i = 0; i < k; i++) {
+		n *= 10;
+		sum += n;
+	}
+	cout << sum;
+
+	return 0;
+}
