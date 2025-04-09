f69ee9ddb5385f559d6c6a847b41ef3f807f4221
simgksk <123716081+simgksk@users.noreply.github.com>
2025-04-09 15:54:17 +0900

    20254. Site Score
    https://www.acmicpc.net/problem/20254

---
+++ Cpp/20xxx/20254.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int ur, tr, uo, to;
+
+	cin >> ur >> tr >> uo >> to;
+
+	int res = (56 * ur) + (24 * tr) + (14 * uo) + (6 * to);
+
+	cout << res;
+
+	return 0;
+}
