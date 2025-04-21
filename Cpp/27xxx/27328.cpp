6162ade2fe804a41de36b4515dc505bd9567e2c7
simgksk <123716081+simgksk@users.noreply.github.com>
2025-04-21 15:58:37 +0900

    27328. 三方比較 (Three-Way Comparison)
    https://www.acmicpc.net/problem/27328

---
+++ Cpp/27xxx/27328.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+
+	cin >> a >> b;
+
+	if (a > b)
+		cout << 1;
+	else if (a < b)
+		cout << -1;
+	else
+		cout << 0;
+
+	return 0;
+}
