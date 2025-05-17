03ce5bcd450021cb2f3eb410273a8804ded9c203
simgksk <123716081+simgksk@users.noreply.github.com>
2025-05-17 23:36:01 +0900

    26574. Copier
    https://www.acmicpc.net/problem/26574

---
+++ Cpp/26xxx/26574.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int num;
+    
+    cin >> n;
+    
+    while(n--){
+        cin >> num;
+        cout << num << " " << num << "\n";
+    }
+}
