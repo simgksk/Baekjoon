a538f539779020f7ed19dba7b463271154a82cda
simgksk <123716081+simgksk@users.noreply.github.com>
2025-08-28 09:42:56 +0900

    1940. 주몽
    https://www.acmicpc.net/problem/1940

---
+++ Cpp/0_1xxx/1940.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, m, num;
+	int cnt = 0;
+	vector<int> vec;
+
+	cin >> n >> m;
+
+	while (n--)	{
+		cin >> num;
+
+		vec.push_back(num);
+	}
+
+	sort(vec.begin(), vec.end());
+
+	int left = 0, right = vec.size() - 1;
+
+	while (left < right) {
+		int sum = vec[left] + vec[right];
+
+		if (sum == m) {
+			cnt++;
+
+			left++;
+			right--;
+		}
+		else if (sum < m)
+			left++;
+		else
+			right--;
+	}
+
+	cout << cnt;
+}
