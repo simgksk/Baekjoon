cbf09a667934afceb8612cc10229dfac03f60d45
simgksk <123716081+simgksk@users.noreply.github.com>
2025-09-11 17:21:27 +0900

    18411. 試験 (Exam)
    https://www.acmicpc.net/problem/18411

---
+++ Cpp/18xxx/18411.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int arr[3] = {};
+
+	for (int i = 0; i < 3; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + 3);
+
+	cout << arr[2] + arr[1];
+
+	return 0;
+}
